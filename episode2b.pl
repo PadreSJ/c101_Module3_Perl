4bda66b4e1f70f1f2125c651199ce9a3ceea9cc0
PadreSJ <padre@twit.tv>
2014-06-06 00:20:34 -0700

    Create episode2b.pl

---
+++ episode2b.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+print "What is your favorite animal?\n";
+$animal = <STDIN>;
+
+$animal =~ s/cat/bunny/gi;
+
+if (($animal =~ /bunny/gi) || ($animal =~ /rabbit/gi)) {
+    print "You said $animal";
+    print "Yes, bunnies are the best!\n"
+} else {
+    print "You said $animal";
+    print "Okay, whatever.\n";
+}
+
+exit;
