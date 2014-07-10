a4a0df006fbf51f55d0b3fb108ca4a83d68789a4
PadreSJ <padre@twit.tv>
2014-07-10 16:38:30 -0700

    Create episode5b.pl

---
+++ episode5b.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# set content type for the page
+print "Content-type: text/html\n\n";
+
+# print the HTML and text
+print <<EOF;
+<html>
+<body>
+<font size="5">Hello World!</font>
+</body>
+</html>
+EOF
