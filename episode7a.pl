5117ebda3ffb8fae1897f1231f9504d97b244038
PadreSJ <padre@twit.tv>
2014-07-24 12:52:56 -0700

    Create episode7a.pl

---
+++ episode7a.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+# set the content type for the page
+print "Content-type: text/html\n\n";
+
+# set the HTML header
+print <<EOF;
+<HTML>
+<HEAD>
+<TITLE>Coding 101 Episode 27</TITLE>
+</HEAD>
+<BODY>
+<H1>Coding 101</H1>
+<P>This is Example A result...</P>
+EOF
+
+read(STDIN,$input,$ENV{'CONTENT_LENGTH'});
+
+print "<P><I>STDIN says: $input</I></P>\n";
+
+if (length ($input) > 0){
+    @things = split(/&/, $input);
+    foreach $thing (@things){
+        ($name, $value) = split(/=/, $thing);
+        $in{$name} = $value; 
+    }
+}
+
+if ($in{'animal'}) {
+    $animal = $in{'animal'};
+} else {
+    $animal = "nothing";
+}
+
+if (($animal =~ /bunny/i) || ($animal =~ /rabbit/i)) {
+    # If they said anything with "bunny" or "rabbit"...
+    print "You said $animal!<br />\n";
+    print "Yes, bunnies are the best!\n";
+} else {
+    # If they didn't and are lame...
+    print "You said $animal...<br />\n";
+    print "Okay, whatever.\n";
+}
+
+# end the HTML
+print <<EOF;
+<P><A HREF="/ep27a.html">Submit again</A></P>
+</BODY>
+</HTML>
+EOF
+
+exit;
